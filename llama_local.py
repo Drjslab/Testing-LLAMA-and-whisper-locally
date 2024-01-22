ee75dcdb3ad3673e2b0103453f6a364ec6955652
J. Prajapati <jig4physics@gmail.com>
2024-01-22 06:16:23 +0530

    Create llama_local.py

---
+++ llama_local.py
@@ -0,0 +1,33 @@
+from langchain.llms import LlamaCpp
+# from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.prompts import PromptTemplate
+
+
+model_file = "llama-2-7b-chat.Q4_K_M.gguf"
+
+template_llama = """<s>[INST] <<SYS>>
+                    You are a smart mini computer named Raspberry Pi.
+                    Write a short but funny answer.</SYS>>
+                    {question} [/INST]"""
+
+template = template_llama
+
+n_gpu_layers = 30  # Change this value based on your model and your GPU VRAM pool.
+n_batch = 512  # Should be between 1 and n_ctx, consider the amount of VRAM in your GPU.
+
+# callback_manager = CallbackManager([StreamingCustomCallbackHandler()])
+llm = LlamaCpp(
+    model_path=model_file,
+    temperature=0.1,
+    n_gpu_layers=n_gpu_layers,
+    n_batch=256,
+    verbose=True,
+)
+
+# llm_chain =LLMChain(prompt=template, llm=llm)
+question = "Who is president of india?"
+# llm_chain.run(question)
+print("Wait...")
+out = llm(question)
+print(out)
+print("--done--")
